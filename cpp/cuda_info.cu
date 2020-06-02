39129d135b9e007cf67c0359ffc3d94246608a2b
unknown <micoss72@gmail.com>
2020-06-02 02:29:54 -0400

    show gpu block and thread info

--- cpp/cuda_info.cu
+++ cpp/cuda_info.cu
@@ -3,28 +3,36 @@
 
 #include <iostream>
 using namespace std;
+__global__ void GPU_mt_info()
+{
+	printf("Block idx: %d | thread idx: %d\n", blockIdx.x, threadIdx.x);
+}
 
-int main(void)
+int main()
 {
 
- '
-    int device_count;
-    
-    cudaGetDeviceCount(&device_count);
-    cout << "CUDA device count " << device_count << endl;
-    
-    cudaDeviceProp device_props;
-    cudaGetDeviceProperties(&device_props, 0);
-    cout << "CUDA first device name: " << device_props.name << endl;
+	int device_count;
 
-    int driver_version;
-    cudaDriverGetVersion(&driver_version);
-    cout << "CUDA driver version: " << driver_version << endl;
+	cudaGetDeviceCount(&device_count);
+	cout << "CUDA device count " << device_count << endl;
 
-    int runtime_version;
-    cudaRuntimeGetVersion(&runtime_version);
-    cout << "CUDA runtime version: " << runtime_version << endl;
+	cudaDeviceProp device_props;
+	cudaGetDeviceProperties(&device_props, 0);
+	cout << "CUDA first device name: " << device_props.name << endl;
 
-    cout << "maxThreadsPerBlock: " << device_props.maxThreadsPerBlock << endl;    int device;
+	int driver_version;
+	cudaDriverGetVersion(&driver_version);
+	cout << "CUDA driver version: " << driver_version << endl;
+
+	int runtime_version;
+	cudaRuntimeGetVersion(&runtime_version);
+	cout << "CUDA runtime version: " << runtime_version << endl;
+
+	cout << "maxThreadsPerBlock: " << device_props.maxThreadsPerBlock << endl;
+
+	cout << "CUDA Total global mem: " << device_props.totalGlobalMem / (1048576.0) << " MB" << endl;
+
+	GPU_mt_info << <2, 10 >> > ();
+	cudaDeviceSynchronize();
 
 }
