68b5e436576c19341a7319c54abde2c6ae1d9fdf
unknown <micoss72@gmail.com>
2020-06-03 21:49:12 -0400

    Exchange and increment a number host/device

--- cpp/cuda_info.cu
+++ cpp/cuda_info.cu
@@ -8,6 +8,36 @@ __global__ void GPU_mt_info()
 	printf("Block idx: %d | thread idx: %d\n", blockIdx.x, threadIdx.x);
 }
 
+
+
+__global__ void GPU_increment_number(int* buffer, int initial)
+{
+    buffer[0] = 1 + initial;
+}
+
+void simple_exchange()
+{
+    int* buffer;
+    const auto BUF_SIZE{ 1 * sizeof(int) };
+
+    int* host_buffer = (int*)malloc(BUF_SIZE);
+    *host_buffer = 99;
+
+    cudaMalloc((void**)&buffer, BUF_SIZE);
+    cudaMemcpy(buffer, host_buffer, BUF_SIZE, cudaMemcpyHostToDevice);
+
+   GPU_increment_number <<< 1, 1 >>> (buffer, 5);
+   
+    cudaDeviceSynchronize();
+
+    cudaMemcpy(host_buffer, buffer, BUF_SIZE, cudaMemcpyDeviceToHost);
+    printf("Incrementing result is %d \n", host_buffer[0]);
+
+    cudaFree(buffer);
+    free(host_buffer);
+}
+
+
 int main()
 {
 
